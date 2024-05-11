d82444a92ebbf359dc79064ae430f01e13c10a54
ZoreAnuj <95142805+ZoreAnuj@users.noreply.github.com>
2024-05-11 14:28:57 -0400

    Update get_data.ino

--- get_data.ino
+++ get_data.ino
@@ -1,30 +1,4 @@
-/*
-  Get Speed/Heading and dilution of precision via UBX binary commands - no more NMEA parsing!
-  By: Nathan Seidle
-  SparkFun Electronics
-  Date: January 3rd, 2019
-  License: MIT. See license file for more information but you can
-  basically do whatever you want with this code.
 
-  This example shows how to query a u-blox module for its lat/long/altitude.
-
-  Note: Long/lat are large numbers because they are * 10^7. To convert lat/long
-  to something google maps understands simply divide the numbers by 1,000,000. We
-  do this so that we don't have to use floating point numbers.
-
-  Leave NMEA parsing behind. Now you can simply ask the module for the datums you want!
-
-  Feel like supporting open source hardware?
-  Buy a board from SparkFun!
-  ZED-F9P RTK2: https://www.sparkfun.com/products/15136
-  NEO-M8P RTK: https://www.sparkfun.com/products/15005
-  SAM-M8Q: https://www.sparkfun.com/products/15106
-
-  Hardware Connections:
-  Plug a Qwiic cable into the GNSS and a BlackBoard
-  If you don't have a platform with a Qwiic connection use the SparkFun Qwiic Breadboard Jumper (https://www.sparkfun.com/products/14425)
-  Open the serial monitor at 115200 baud to see the output
-*/
 
 #include <Wire.h> //Needed for I2C to GNSS
 
